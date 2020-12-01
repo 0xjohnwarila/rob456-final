cc82e651f2b00b537128810725444f4f92c173d8
JohnWarila <johnwarila@gmail.com>
2020-12-01 13:09:00 -0800

    priority queue test

--- astar.py
+++ astar.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python2
+import heapq
 
-import matplotlib.pyplot as plt
-import numpy as np
+# queue
+q = []
 
-print "test"
+# (weight, edge)
+heapq.heappush(q, (2, 'pt1'))
+heapq.heappush(q, (1, 'pt2'))
+heapq.heappush(q, (3, 'pt3'))
+heapq.heappush(q, (10, 'pt4'))
+heapq.heappush(q, (2.4, 'pt5'))
+
+while q:
+    next_item = heapq.heappop(q)
+    print next_item
