022229dd5a051360f4b75761a0bcad0c52fbee73
JohnWarila <johnwarila@gmail.com>
2020-12-01 23:18:49 -0800

    comments

--- floodfill.py
+++ floodfill.py
@@ -4,25 +4,38 @@ import mapreading
 import heapq as hq
 
 def floodfill(m, target):
+    # Setup data structures
+
+    # priority queue
     q = []
+    
+    # points and their priority
     points = {}
     closed = set()
+
+    # put the first node in the queue
     hq.heappush(q, (0, target))
     points[target] = 0
 
     while q:
+        
+        # get lowest priority (but really high for the algo)
         curr = hq.heappop(q)
         closed.add(curr[1])
+        # get neighbors
         neighbors = mapreading.neighbors_explored(m, curr[1])
         for n in neighbors:
             neighbor = (n[0], n[1])
             if neighbor in closed:
                 continue
+            # new priority
             new_priority = curr[0] + 1
             if points.has_key(neighbor):
+                # update priority
                 if points[neighbor] > new_priority:
                     points[neighbor] = new_priority
             else:
+                # add new node
                 points[neighbor] = new_priority
                 hq.heappush(q, (new_priority, neighbor))
 
@@ -42,3 +55,4 @@ if __name__ == '__main__':
 
     plt.imshow(img)
     plt.show()
+
